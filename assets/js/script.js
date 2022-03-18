f25756f41ad61010aec0c90ce0f7e2a159a4a74d
Baeonetta <victor.m.kennedy@gmail.com>
2022-03-17 22:19:23 -0500

    Corrected the smaller cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
-var searchHistory =  []; 
+var searchHistory =  [];
+let userInput = $("#search")
 const weatherApiUrl = "https://api.openweathermap.org";
 const apiKey = "d57770cf834ac891e7e282fc1655e324";
 const bigCard = $("#big-card-h");
@@ -49,9 +50,20 @@ async function getApi() {
       showWeatherData();
       renderCards();
 }
-    
-    subBtn.click(function(event){
+let searches = []
+function pastSearch(){   
+for (var i = 0; i < searches.length; i++){
+  searches.push(userInput.val())
+}
+localStorage.setItem("history", JSON.stringify(userInput.val()));
+console.log(searches)
+}    
+
+
+subBtn.click(function(event){
         event.preventDefault();
+        pastSearch();
+
 });    
 // Readme asks for  temperature, the humidity, the wind speed, and the UV index
 function showWeatherData(){
@@ -64,7 +76,7 @@ function showWeatherData(){
    let humidity =  weather.daily[0].humidity;
    let icon = weather.daily[0].weather[0].icon
    // Place all the info on the page, in the bigger Card for the main data
-   bigCard.html(
+   bigCard.append(
    `<div  class="card-header">${city}</div>
     <div class="card-body"><h4 class="card-title">${status}</h4>
     <div class="card-body"><h4 class="card-title"><img src="http://openweathermap.org/img/wn/${icon}@2x.png" alt="weather icon"></h4>
@@ -73,29 +85,50 @@ function showWeatherData(){
     <p class="card-text">Windspeed: ${windSpeed} mph</p>
     </div>`)
 };
+// console.log(weather.daily)
+// weather.daily.forEach(idx => {
+//   if (idx == 0) {
+
+//   }else renderCards
+
+  
+// });
 
 function renderCards(){
-    let i = 0;
-    // for (let i = 0; i < data.list.length; i++){
-      let status = weather.daily[i].weather[i].description
-      let windSpeed = weather.daily[i].wind_speed;
-      let temp = weather.daily[i].temp.day;
-      let humidity =  weather.daily[i].humidity;
-      let icon = weather.daily[i].weather[i].icon
-    // render the small cards like the bigger one with modified data
-    smallCard.html(
-    `<div class="card">
-    <div class="card-body">
-      <h4 class="card-title">date goes here</h4>
-      <p class="card-text"><img src="http://openweathermap.org/img/wn/${icon}@2x.png" alt="weather icon"></p>
-      <p class="card-text">Temp: ${temp}</p>
-      <p class="card-text">Humidity: ${humidity}</p>
-      <p class="card-text">Wind Speed: ${windSpeed}</p>
-    </div>
-  </div>`)};
+  console.log(weather.daily.length);
+  for (i = 0; i < weather.daily.length -3; i++) {
+    // console.log(i);
+  let status = weather.daily[i].weather[0].description;
+  console.log(status) 
+  }
+  console.log(weather.daily[0]);
+  weather.daily.forEach((weekday) => {
+    // console.log(weekday.temp.day);
+        let status = weekday.weather.description;
+        let windSpeed = weekday.wind_speed;
+        let temp = weekday.temp.day;
+        let humidity =  weekday.humidity;
+        let icon = weekday.weather[0].icon;
+        // render the small cards like the bigger one with modified data
+        smallCard.append(
+          `<div class="card">
+          <div class="card-body">
+          <h4 class="card-title">date goes here</h4>
+          <p class="card-text"><img src="http://openweathermap.org/img/wn/${icon}@2x.png" alt="weather icon"></p>
+          <p class="card-text">Temp: ${temp}</p>
+          <p class="card-text">Humidity: ${humidity}%</p>
+          <p class="card-text">Wind Speed: ${windSpeed} mph</p>
+          </div>`)
+    });
+  };
+  // for (weekday starting at the first value, 8, go until you reach 8)
+
+
+
+
     
 
-geoGrab();
+
 getApi();
 // create the search feature
 
