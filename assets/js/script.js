6f39132226bbbd5e05dd9816d5001e34a83fb9d9
Baeonetta <victor.m.kennedy@gmail.com>
2022-03-18 19:10:40 -0500

    finalized stylings of page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,19 @@
-var searchHistory =  [];
+var searchHistory ={ city: []};
 let userInput = $("#search")
 const weatherApiUrl = "https://api.openweathermap.org";
 const apiKey = "d57770cf834ac891e7e282fc1655e324";
 const bigCard = $("#big-card-h");
 const subBtn = $("#submitbutton");
 const smallCard =$("#days"); 
+const removeCard =$("smallcard")
+const pastbutton = $("#history")
 let coordinates;
 let weather;
-// get the api to pull results
 
+// get the api to pull results
 async function geoGrab() {
-  //  let city = getLocation;
-  let geoURL = `https://api.openweathermap.org/geo/1.0/direct?q=dallas&appid=${apiKey}`;
+ 
+  let geoURL = `https://api.openweathermap.org/geo/1.0/direct?q=${searches}&appid=${apiKey}`;
   
   await fetch(geoURL)
   .then(function(response) {
@@ -28,17 +30,15 @@ async function geoGrab() {
 
 
 async function getApi() {
-    await geoGrab();
-    // console.log(coordinates);
+    await geoGrab(searches);
+    console.log(searches);
 
-    // let {lat, lon} = [coordinates[0].lat, coordinates[0].lon];
-    // console.log(coordinates[0].lat)
-    // console.log(coordinates[0].lon)
+ 
     let queryURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates[0].lat}&lon=${coordinates[0].lon}&units=imperial&appid=${apiKey}`;
 
     await fetch(queryURL)
       .then(function(response) {
-          console.log(response.statusText);
+          // console.log(response.statusText);
           return response.json();
       })
       .then(function(data) {
@@ -49,87 +49,86 @@ async function getApi() {
       
       showWeatherData();
       renderCards();
+      searchButtons();
 }
-let searches = []
-function pastSearch(){   
-for (var i = 0; i < searches.length; i++){
-  searches.push(userInput.val())
-}
-localStorage.setItem("history", JSON.stringify(userInput.val()));
-console.log(searches)
-}    
 
 
 subBtn.click(function(event){
-        event.preventDefault();
-        pastSearch();
+  event.preventDefault();
+  searches = userInput.val();
+  console.log(searches);
+  saveSearches(searches);
+  getApi(searches);
+});   
 
-});    
+function saveSearches(searches){
+  searchHistory.city.push(searches)
+  localStorage.setItem("city", JSON.stringify(searchHistory));
+}
+
+function loadSearches(){
+  if(localStorage.getItem("city")) {
+    searchHistory = JSON.parse(localStorage.getItem("city"));
+    // console.log(searchHistory);
+    searchButtons(searchHistory);
+  }
+}
+
+
+
+function searchButtons(){
+  for (i = 0; i < searchHistory.city.length; i++){
+    if (i > searchHistory.city.length -6){
+    let btnTxt = searchHistory.city[i];
+  pastbutton.append(`<button type="button" id="past" class="btn 
+  btn-outline-dark btn-lg btn-block">${searchHistory.city[i]}</button>`)
+  }
+}};
+
+
+
+
 // Readme asks for  temperature, the humidity, the wind speed, and the UV index
 function showWeatherData(){
-  console.log(weather)
-  //  let date = weather
-   let status = weather.daily[0].weather[0].description
+   let date = moment();
+   let status = weather.daily[0].weather[0].description;
    let city = coordinates[0].name;
    let windSpeed = weather.daily[0].wind_speed;
    let temp = weather.daily[0].temp.day;
    let humidity =  weather.daily[0].humidity;
    let icon = weather.daily[0].weather[0].icon
    // Place all the info on the page, in the bigger Card for the main data
-   bigCard.append(
-   `<div  class="card-header">${city}</div>
+   bigCard.html(
+   `<div id="bigcard" class="card">
+    <div class="card-header text-center">${city}, ${date.format("MMMM Do")}</div>
     <div class="card-body"><h4 class="card-title">${status}</h4>
     <div class="card-body"><h4 class="card-title"><img src="http://openweathermap.org/img/wn/${icon}@2x.png" alt="weather icon"></h4>
-    <p class="card-text">Temperature: ${temp}</p>
+    <p class="card-text">Temperature: ${temp}°</p>
     <p class="card-text">Humidity: ${humidity}%</p>
     <p class="card-text">Windspeed: ${windSpeed} mph</p>
-    </div>`)
-};
-// console.log(weather.daily)
-// weather.daily.forEach(idx => {
-//   if (idx == 0) {
-
-//   }else renderCards
-
-  
-// });
-
+    </div></div>`)
+};  
+  // for (weekday starting at the first value, 8, go until you reach 8)
 function renderCards(){
-  console.log(weather.daily.length);
-  for (i = 0; i < weather.daily.length -3; i++) {
-    // console.log(i);
-  let status = weather.daily[i].weather[0].description;
-  console.log(status) 
-  }
-  console.log(weather.daily[0]);
+  smallCard.empty()
   weather.daily.forEach((weekday) => {
-    // console.log(weekday.temp.day);
-        let status = weekday.weather.description;
         let windSpeed = weekday.wind_speed;
         let temp = weekday.temp.day;
         let humidity =  weekday.humidity;
         let icon = weekday.weather[0].icon;
         // render the small cards like the bigger one with modified data
         smallCard.append(
-          `<div class="card">
+          `<div id="smallcard" class="card" style="width: 18rem;">
           <div class="card-body">
-          <h4 class="card-title">date goes here</h4>
           <p class="card-text"><img src="http://openweathermap.org/img/wn/${icon}@2x.png" alt="weather icon"></p>
-          <p class="card-text">Temp: ${temp}</p>
+          <p class="card-text">Temp: ${temp}°</p>
           <p class="card-text">Humidity: ${humidity}%</p>
           <p class="card-text">Wind Speed: ${windSpeed} mph</p>
           </div>`)
-    });
-  };
-  // for (weekday starting at the first value, 8, go until you reach 8)
+        })
+};
 
 
-
-
-    
-
-
-getApi();
-// create the search feature
+loadSearches();
 
 
